a497477e949718c9fa334abed5cc6b3681250f23
unknown <pamforlack@gmail.com>
2022-10-25 20:13:05 -0500

    fix

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		string name = "pam";
+		String name = "pam";
 
-		system.out.println(name)
+		System.out.println(name);
 	}
 
 }
